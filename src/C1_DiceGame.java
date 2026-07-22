bffcaf01a84050dfe925037bf58eff7e3a44c734
Aidan Cornett <aidncnett@gmail.com>
2026-07-22 13:00:42 -0500

    Added a lot of methods and fixed a few bugs * fixed the bug where multiple irrelevant error messages would be dsiplayed. * added more methods to Main so it would be easier to read. * planning to make a loading bar but I also need to continue condensing.

--- src/C1_DiceGame.java
+++ src/C1_DiceGame.java
@@ -1,8 +1,10 @@
 /* 
- * Aidan Corentt DiceGame ver: 3
+ * Aidan Corentt DiceGame ver: 4
  * ----------------------------
  * I will put my original pseudocode notes above most of my main functions for readability!
- * I am enjoying this SO much! Basically spent the whole day (7 / 20 /2026) adding small things!
+ * I am enjoying this SO much! Basically spent the whole day (7 / 20 /2026) adding small things and fixing bugs!
+ * 
+ * Version 4: added a bunch of methods and fixed irrelevant errors when getting n from input
  * 
  * I want to put a loading bar, but my first attempt 100x'd my time so research is needed.
  */
@@ -15,11 +17,90 @@ import java.lang.Math;
 import java.text.DecimalFormat;
 
 public class C1_DiceGame {
-    public static void loadingBar (long iteration, long total) {
+    public static long[] roll2Dice(long rolls, Random die, long[] count) {
+        long i;
+        for (i = 1; i <= rolls; ++i) {
+            int value = die.nextInt(6) + die.nextInt(6);
+            count[value] += 1;
+        }
+        return count;
+    }
+
+    public static long[] roll1Die(long rolls, Random die, long[] count) {
+        long i;
+        for (i = 1; i <= rolls; ++i) {
+            int value = die.nextInt(11);
+            count[value] += 1;
+        }
+        return count;
+    }
+    
+    //validates the users input of N
+    public static long validateN (long n, Scanner scnr) {
+        boolean validLong = false;
+        System.out.println("\nHow many times would you like to roll your die / dice?");
+        //check if valid integer
+        try {
+            n = scnr.nextLong();
+            validLong = true;
+        }
+        catch (InputMismatchException e) {
+            System.out.println("> Input is not a valid integer.\n");
+            scnr.next();
+        }
+        
+        //check if 0 or negative
+        if (n <= 0 && validLong == true) {
+            System.out.println("> Negatives or zero values are not valid.\n");
+            return 0;
+        }
+        else if (n > 1_000_000_000_000_000_000L && validLong == true) {
+            System.out.println("> value too large: i.e. larger than 1,000,000,000,000,000,000. (1 quintillion)\n");
+            return 0;
+        }
+        else {
+            return n;
+        }
+    }
+    
+    //prints the total times a certain value is rolled
+    public static void printFrequency (long[] count) {
+        int i;
+        System.out.print("\n----------------------\n" +
+                           "  Frequency of rolls  \n" +
+                           "----------------------\n");
+
+                for (i = 0; i < 11; ++i) {
+                    
+                    //Resulting value formatting
+                    if ((i + 2) < 10) {
+                        System.out.println((i + 2) + ":  " + count[(int)i]);
+                    }
+                    if ((i + 2) >= 10) {
+                        System.out.println((i + 2) + ": " + count[(int)i]);
+                    }
+                }
+    }
+    
+    public static void printTime (long start, long end) {
+        long duration = (end - start) / 1_000_000;
+        if (duration != 0.0) {
+            System.out.println("\n      " + (double)duration / 1000 + " seconds");
+        }
+        else {
+            System.out.println("\n      < 0.000 seconds");
+        }
+        System.out.print("--------- End ----------\n\n");
+        duration = 0;
+    }    
+
+
+    //loading bar method (WIP)
+    public static void loadingBar (String placeholder) {
         //currently does nothing!
     }
 
-    
+
     //set array with all 0's
     public static void zeroArray (long[] array) {
         int i;
@@ -28,26 +109,26 @@ public class C1_DiceGame {
             array[i] = 0;
         }
     }
-    
-    //find array index of max value for histogram creation, this value will == 10 asterisks.
-    public static int findMax(long[] array) {
+
+
+    //find array index of max value for histogram creation, this value will == 25 asterisks.
+    public static int findMax (long[] array) {
         int maxLoc = 0;
         long maxVal = array[(int)0];
         int i;
 
-        
         for (i = 1; i < 11; ++i) {
             if (array[i] > maxVal) {
                 maxLoc = i;
                 maxVal = array[(int)i];
             }
         }
-
         return maxLoc;
     }
 
+
     //print the height of bar (using asterisks), this method, calculates the reltionship (using divison) between the max value and each die value. and prints a histogram of asterisks.
-    public static void printDistribution(long[] array, int indexOfMax) {
+    public static void printDistribution (long[] array, int indexOfMax) {
         int i;
         int j;
         double max = array[indexOfMax];
@@ -76,19 +157,19 @@ public class C1_DiceGame {
         return;
     }
 
-    public static void main(String[] args) {
 
+    //main!!
+    public static void main (String[] args) {
 
         // all main variables ### check before submission ###
         Scanner scnr = new Scanner(System.in);
         Random die = new Random();
-        DecimalFormat Format = new DecimalFormat();
-        // i is a long to prevent overflow
+        DecimalFormat Formater = new DecimalFormat();
+        // i is a long to prevent overflow for very large dice roll amounts
         long i;
         long n = 1;
         long strtTime = 0;
         long endTime = 0;
-        long duration;
         int response = 0;
         int value = 0;
         long[] count = new long[11];
@@ -96,17 +177,17 @@ public class C1_DiceGame {
         boolean isValidR = false;
 
 
-        // set every list item inside "count" to 0
+        // set every item inside arrray "count" to 0
         zeroArray(count);
 
         
         // menu, '1' for set roll count || '2' for 1 die || '3' for 2 die || '4' for exit 
         while (isValidR == false) {
-            System.out.println("\n ----------------- Menu ----------------"   + "\n" +
-                                  "1) Set number of rolls (Current: "+Format.format(n)+")"   + "\n" +
-                                  "2) 1 die (2-12)"                           + "\n" +
-                                  "3) 2 die (1-6) + (1-6)"                    + "\n" +
-                                  "4) Quit program"                           + "\n");
+            System.out.println("\n ----------------- Menu ----------------"                  + "\n" +
+                                  "1) Set number of rolls (Current: "+Formater.format(n)+")"   + "\n" +
+                                  "2) 1 die (2-12)"                                          + "\n" +
+                                  "3) 2 die (1-6) + (1-6)"                                   + "\n" +
+                                  "4) Quit program"                                          + "\n");
             
             
             // R input validation
@@ -131,26 +212,11 @@ public class C1_DiceGame {
                 
                 // ask user how many times they want to roll their die / dice, then validate their input and save to n.
                 while (isValidN == false) {
-                    System.out.println("\nHow many times would you like to roll your die / dice?");
-                    
-                    // N input validation
-                    try {
-                        n = scnr.nextLong();
-                    }
-                    catch (InputMismatchException e) {
-                        System.out.println("> Input is not a valid integer.\n");
-                        scnr.next();
-                    }
-                    
-                    if (n <= 0) {
-                        System.out.println("> Negatives or zero values are not valid.\n");
-                    }
-                    else if (n > 1_000_000_000_000_000_000L) {
-                        System.out.println("> value too large: i.e. larger than 1,000,000,000,000,000,000. (1 quintillion)\n");
-                    }
-                    else {
+                    n = validateN(n, scnr);
+                    if (n != 0) {
                         isValidN = true;
                     }
+                    //returns user to main menu
                     isValidR = false;
                 }
             }
@@ -163,39 +229,17 @@ public class C1_DiceGame {
                 strtTime = System.nanoTime();
                 isValidR = true;
                 
-                //value selection (2 - 12) -------- weird for loop structure to avoid division of 0 in loading bar
-                for (i = 1; i <= n; ++i) {
-                    value = die.nextInt(11);
-                    count[value] += 1;
-                }
+                //value selection (2 - 12)
+                count = roll1Die(n, die, count);
                 
-
-                System.out.print("\n----------------------\n" +
-                                   "  Frequency of rolls  \n" +
-                                   "----------------------\n");
-
-                for (i = 0; i < 11; ++i) {
-                    
-                    //Resulting value formatting
-                    if ((i + 2) < 10) {
-                        System.out.println((i + 2) + ":  " + count[(int)i]);
-                    }
-                    if ((i + 2) >= 10) {
-                        System.out.println((i + 2) + ": " + count[(int)i]);
-                    }
-                }
-                
+                // value and curve printing
+                printFrequency(count);
                 printDistribution(count, findMax(count));
-                
-                //time printing
+                // time printing
                 endTime = System.nanoTime();
-                duration = (endTime - strtTime) / 1_000_000;
-                System.out.println("\n      " + (double)duration / 1000 + " seconds");
-                System.out.print("--------- End ----------\n\n");
-                
-                //reset
+                printTime(strtTime, endTime);
+                // reset
                 zeroArray(count);
-                duration = 0;
                 isValidR = false;
             }
 
@@ -206,42 +250,18 @@ public class C1_DiceGame {
                 strtTime = System.nanoTime();
                 isValidR = true;
                 
-                //value selection (1 - 6) + (1 - 6) -------- weird for loop structure to avoid division of 0 in loading bar
-                for (i = 1; i <= n; ++i) {
-                    value = die.nextInt(6) + die.nextInt(6);
-                    count[value] += 1;
-                }
+                //value selection (1 - 6) + (1 - 6)
+                count = roll2Dice(n, die, count);
 
-                System.out.print("\n----------------------\n" +
-                                   "  Frequency of rolls  \n" +
-                                   "----------------------\n");
-                for (i = 0; i < 11; ++i) {
-
-                    //resulting value formatting
-                    if ((i + 2) < 10) {
-                        System.out.println((i + 2) + ":  " + count[(int)i]);
-                    }
-                    if ((i + 2) >= 10) {
-                        System.out.println((i + 2) + ": " + count[(int)i]);
-                    }
-                }
-
+                printFrequency(count);
                 printDistribution(count, findMax(count));
                 
                 //time printing
                 endTime = System.nanoTime();
-                duration = (endTime - strtTime) / 1_000_000;
-                if (duration != 0.0) {
-                    System.out.println("\n      " + (double)duration / 1000 + " seconds");
-                }
-                else {
-                    System.out.println("\n      < 0.000 seconds");
-                }
-                System.out.print("--------- End ----------\n\n");
+                printTime(strtTime, endTime);
                 
                 //reset
                 zeroArray(count);
-                duration = 0;
                 isValidR = false;
             }
 
